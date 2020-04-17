06e0fb074f183e1f93e57425694ec22d36703e3e
Brian Lee <makewook88@gmail.com>
2020-04-17 14:05:44 +0900

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 00:52:26 2019
+
+@author: Brian
+"""
+
+class Calculator:
+    def __init__(self,numberList):
+        self.numberList = numberList
+        
+    def sum(self):
+        result = 0
+        for num in self.numberList:
+            result += num
+        return result
+    
+    def avg(self):
+        total = self.sum()
+        return total / len(self.numberList)
+    
+    
+    
+cal1 = Calculator([1,2,3,4,5])
+print(cal1.sum())
+print(cal1.avg())
+
+cal2 = Calculator([6,7,8,9,10])
+print(cal2.sum())
+print(cal2.avg())
